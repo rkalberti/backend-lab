f75721ddbeeca129fa9cdeeb5f6d6f13b6297c5e
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-03-17 17:42:38 -0300

    Atualizar o estoque.js

--- estoque.js
+++ estoque.js
@@ -1,3 +1,17 @@
+/*
+===============ATIVIDADE===============
+1) Crie o nome de 8 produtos e armazene em uma lista;
+2) Crie outra lista para armazenar o preço de cada produto.
+3) Crie uma função para mostrar o estoque, mostrando o nome da cada produto e o seu preço.
+4) Crie uma função que adiciona um novo produto (e consequentemente um novo preço)
+5) Crie uma função que remove um produto da lista recebendo seu índice.
+6) EXECUTE AS FUNÇÕES CRIADAS NA SEGUINTE ORDEM:
+    - mostra estoque
+    - adiciona produto
+    - mostra estoque
+    - remove um elemento pelo índice
+    - mostra estoque
+*/
 const produtos = [
     "Memória RAM DDR4 8GB",  "Memória RAM DDR5 16GB",  "SSD Kingston",  "Ryzen 7 7700",   "Intel i5-12400F",   "Ryzen 5 5500",  "RTX 4090", "RX6600"
 ];
