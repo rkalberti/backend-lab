2ecfcb9636d5ce5d2b8be3db5cbb9ed185d52fc1
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-21 21:15:10 -0300

    Criar aula-tres.js

---
+++ primeiro-tri/aula-tres.js
@@ -0,0 +1,22 @@
+// BOOLEANOS: "true" e "false"
+/* Operadores de comparação:
+== -> igualdade
+!= -> desigualdade
+>  -> maior que
+<  -> menor que
+>= -> maior ou igual que
+<= -> menor ou igual que
+
+* Operador de igualdade estrita *
+=== -> igualdade estrita (compara conteúdo e tipo da variável)
+!== -> desigualdade estrita
+*/
+console.log(10 == "10")
+console.log(true == 1)
+console.log(false == 0)
+
+// Igualdade estrita
+
+console.log(1 === "1")
+console.log(true === 1)
+console.log(false === 0)
