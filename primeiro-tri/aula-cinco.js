43e4f4a4dd595fa35c23df5d280dbd552f00c811
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-21 21:40:13 -0300

    Atualizar o aula-cinco.js

--- primeiro-tri/aula-cinco.js
+++ primeiro-tri/aula-cinco.js
@@ -1,21 +1,31 @@
 // Listas
 // índice: sempre começa em 0
-//                    0           1           2            3
+
+
 const lanches = ["X-Beronha", "X-Ratão", "X-Cachoro", "X-Podrão"]
 const valores = [    24     ,     26   ,      12    ,     31    ]
+
+
+
+// WHILE → repete enquanto a condição for verdadeira
+
 let contador = 0 // variável de início
 while(contador < lanches.length){ // condição de parada
     console.log(lanches[contador] + " - R$ " + valores[contador])
     contador = contador + 1 // controle do laço
 }
+
+
 // Adicionar novos elementos na lista
 lanches.push("X-Sapão")
 valores.push(25)
 console.log(lanches)
+
 // Remover o último
 lanches.pop()
 valores.pop()
 console.log(lanches)
+
 // Remover um item específico ou adicionar
 lanches.splice(2, 1) // remove a partir do índice 2, 1 elemento só
 console.log(lanches)
