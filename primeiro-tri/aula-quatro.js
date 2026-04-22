1acacd0ace797932f10851af1f265a2fb38faca9
Ketlyn Alberti <alberti.ketlyn@gmail.com>
2026-04-21 21:17:27 -0300

    Criar aula-quatro.js

---
+++ primeiro-tri/aula-quatro.js
@@ -0,0 +1,44 @@
+// Condições
+// if ... else
+
+
+const idade = 20
+if (idade < 18) {
+    console.log("Você não pode tirar a CNH")
+} else {
+    console.log("Você pode tirar CNH, pague a taxa!")
+}
+
+const possuiCNH = false
+if (idade >= 18 && possuiCNH == true){
+    console.log("Tudo certo, você pode dirigir!")
+}
+
+
+// Operadores lógicos:
+// && -> E
+// || -> OU
+/* TABELA VERDADE:
+Para operador E:    |    operador OU:
+V V = V             |   V V = V
+V F = F             |   V F = V
+F V = F             |   F V = V
+F F = F             |   F F = F
+*/
+
+
+
+/* A escola adota uma recuperação para cada avaliação, 
+que substitui a nota do aluno caso seja maior que 
+sua nota anterior.
+Dada as seguintes notas, calcule a média simples 
+do aluno e diga se ele está aprovado ou reprovado, 
+levando em consideração a substituição de notas. */
+
+const av1 = 7.0;
+const av2 = 5.0;
+const recup1 = 4.0;
+const recup2 = 10.0;
+let nota1;
+let nota2;
+let media;
